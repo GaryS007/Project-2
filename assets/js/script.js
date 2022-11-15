a35e20453ba00c16cd8bc75d88989fc3d6c40eb3
GaryS007 <chattomeh@hotmail.com>
2022-11-15 20:29:43 +0000

    Add Modal Window and ability to close it using JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-console.log("hello")
+// Wait for DOM to finish loading before running the game
+
+function closeModal() {
+    modal.style.display = "none";
+}
+
+let modal = document.getElementById("main-modal");
+let btn = document.getElementById("modal-button");
+
+btn.addEventListener('click', function() {
+    closeModal()
+})
+
