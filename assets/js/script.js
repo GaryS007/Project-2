6f972fa151da88bc69567f15165ebdfaaf1af9e2
GaryS007 <chattomeh@hotmail.com>
2022-11-16 15:28:17 +0000

    Add Javascript for rock, paper and scissors buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Wait for DOM to finish loading before running the game
+// closeModal allows you to close the Modal when you Start Game
 
 function closeModal() {
     modal.style.display = "none";
@@ -11,3 +11,35 @@ btn.addEventListener('click', function() {
     closeModal()
 })
 
+// runGame function is designed to implement which choice the player takes.
+
+let playerBox = document.getElementById('player-choice');
+
+function rockChoice() {
+    playerBox.innerHTML = '<i class="fa-solid fa-hand-back-fist"></i>';
+}
+
+function paperChoice() {
+    playerBox.innerHTML = '<i class="fa-solid fa-paper-plane"></i>';
+}
+
+function scissorsChoice() {
+    playerBox.innerHTML = '<i class="fa-solid fa-scissors"></i>';
+}
+
+let rock = document.getElementById("rock");
+let paper = document.getElementById("paper");
+let scissors = document.getElementById("scissors");
+
+
+rock.addEventListener('click', function() {
+    rockChoice()
+})
+
+paper.addEventListener('click', function() {
+    paperChoice()
+})
+
+scissors.addEventListener('click', function() {
+    scissorsChoice()
+})
