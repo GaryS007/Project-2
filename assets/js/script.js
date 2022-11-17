14b3a61b1938af17f27be44de6eb3cf8c678ff6e
GaryS007 <chattomeh@hotmail.com>
2022-11-17 13:10:10 +0000

    Add chooseWinner function to compare the results and display winner

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,17 +13,22 @@ btn.addEventListener('click', function() {
 
 // Player Choice inserts rock/paper/scissors icon
 
+let player;
+
 let playerBox = document.getElementById('player-choice');
 
 function rockChoice() {
+    player = 'rock';
     playerBox.innerHTML = '<i class="fa-solid fa-hand-back-fist"></i>';
 }
 
 function paperChoice() {
+    player = 'paper';
     playerBox.innerHTML = '<i class="fa-solid fa-paper-plane"></i>';
 }
 
 function scissorsChoice() {
+    player = 'scissors';
     playerBox.innerHTML = '<i class="fa-solid fa-scissors"></i>';
 }
 
@@ -38,40 +43,52 @@ let scissors = document.getElementById("scissors");
 rock.addEventListener('click', function() {
     rockChoice();
     removeChoiceAi();
+    removeResults();
     computerChoiceDelay();
 })
 
 paper.addEventListener('click', function() {
     paperChoice();
     removeChoiceAi();
+    removeResults();
     computerChoiceDelay();
 })
 
 scissors.addEventListener('click', function() {
     scissorsChoice();
     removeChoiceAi();
+    removeResults();
     computerChoiceDelay();
 })
 
 
 // Computer Choices insert rock/paper/scissors after a 1 second delay
 
+let computer;
+
 let computerBox = document.getElementById('computer-choice');
 
 function rockChoiceAi() {
+    computer = 'rock';
     computerBox.innerHTML = '<i class="fa-solid fa-hand-back-fist"></i>';
 }
 
 function paperChoiceAi() {
+    computer = 'paper';
     computerBox.innerHTML = '<i class="fa-solid fa-paper-plane"></i>';
 }
 
 function scissorsChoiceAi() {
+    computer = 'scissors';
     computerBox.innerHTML = '<i class="fa-solid fa-scissors"></i>';
 }
 
 function removeChoiceAi() {
     computerBox.innerHTML = '';
+}
+
+function removeResults() {
+    results.innerHTML = '';
 }
 
 let computerOptions = [ 
@@ -95,8 +112,38 @@ function computerChoice() {
 
 
 function computerChoiceDelay() {
-    setTimeout(computerChoice, 500);
-}
+    setTimeout(
+        function() {
+            computerChoice();
+            chooseWinner();
+        }, 500);
+    }
 
 // Calculates who the winner is
 
+function chooseWinner() {
+    if (player == computer) {
+        resultsDraw();
+    } else if (computer == 'rock'){
+        (player == 'paper') ? resultsWin() : resultsLose()
+    } else if (computer == 'paper'){
+        (player == 'scissors') ? resultsWin() : resultsLose()
+    } else if (computer == 'scissors'){
+        (player == 'rock') ? resultsWin() : resultsLose()
+    }
+}
+
+let results = document.getElementById('results');
+
+function resultsWin() {
+    results.innerHTML = '<h4>You Win!</h4>';
+
+}
+
+function resultsLose() {
+    results.innerHTML = '<h4>You Lose!</h4>';
+}
+
+function resultsDraw() {
+    results.innerHTML = '<h4>DRAW!</h4>';
+}
